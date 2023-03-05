b47b74c115444d090fc5927317eba6620cfb45e0
jeffery <jeffery.arkar@weborigo.eu>
2023-03-06 00:45:24 +0630

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,36 @@
 #![allow(unused)]
 
-use std::io;
 use rand::Rng;
-use std::io::{Write,BufReader,BufRead,ErrorKind};
-use std::fs::File;
 use std::cmp::Ordering;
+use std::fs::File;
+use std::io::{BufRead, BufReader, ErrorKind, Write};
+use std::{array, io};
 
 fn main() {
-   let mut my_age =202;
+    let mut my_age = 202;
 
-   let is_my_age:bool= if my_age==233 {
-       true
-   }else{
-    false
-   };
-   println!("is my age {}",is_my_age);
+    let is_my_age: bool = if my_age == 233 { true } else { false };
+    println!("is my age {}", is_my_age);
 
-   match my_age {
-       1..=100 => println!("is between 1 and 100"),
-       10|20=> println!("OR"),
-       _ => println!("all equal to default")
-   }
-let voting_age=18;
+    match my_age {
+        1..=100 => println!("is between 1 and 100"),
+        10 | 20 => println!("OR"),
+        _ => println!("all equal to default"),
+    }
+    let voting_age = 18;
 
- match my_age.cmp(&voting_age) {
-    Ordering::Less => println!("CANNOT VOTE"),
-    Ordering::Greater=>println!("CAN VOTE"),
-    Ordering::Equal=>println!("GAIN THE RIGHT TO VOTE")
-
- }
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("CANNOT VOTE"),
+        Ordering::Greater => println!("CAN VOTE"),
+        Ordering::Equal => println!("GAIN THE RIGHT TO VOTE"),
+    }
+    let mut array_1 = [1, 2, 3, 4];
+    let mut index = 0;
+    loop {
+        if (array_1.len() < index + 1) {
+            break;
+        }
+        println!("looping through array {}", array_1[index]);
+        index += 1;
+    }
 }
